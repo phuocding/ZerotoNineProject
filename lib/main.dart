87eba3780b6c42a3697b287975ab6c1a62974144
phuocding <phuocding@gmail.com>
2018-12-11 15:56:12 +0700

    adding a laucnher icon and keystore

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-final dummySnapshots = [
-  {"name": "Tùng", "votes": 20},
-  {"name": "Phước", "votes": 23},
-  {"name": "Anh Duy", "votes": 24},
-  {"name": "Ngọc", "votes": 23},
-  {"name": "Anh Lôi", "votes": 27},
-  {"name": "Chị Đại", "votes": 31},
-  {"name": "Đại Ka", "votes": 33},
-  {"name": "Anh Quốc Anh", "votes": 33},
-  {"name": "Anh Tít", "votes": 28},
-  {"name": "Anh Hoan", "votes": 28},
-];
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,69 +13,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
- class MyHomePage extends StatefulWidget {
-   @override
-   _MyHomePageState createState() {
-     return _MyHomePageState();
-   }
- }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
 
- class _MyHomePageState extends State<MyHomePage> {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(title: Text('Team Votes')),
-       body: _buildBody(context),
-     );
-   }
- }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Impressive Person')),
+      body: _buildBody(context),
+    );
+  }
+}
 
- Widget _buildBody(BuildContext context) {
-   // TODO: get actual snapshot from Cloud Firestore
-   return _buildList(context, dummySnapshots);
- }
+Widget _buildBody(BuildContext context) {
+  // TODO: get actual snapshot from Cloud Firestore
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('members').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return LinearProgressIndicator();
 
- Widget _buildList(BuildContext context, List<Map> snapshot) {
-   return ListView(
-     padding: const EdgeInsets.only(top: 20.0),
-     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-   );
- }
+      return _buildList(context, snapshot.data.documents);
+    },
+  );
+}
 
- Widget _buildListItem(BuildContext context, Map data) {
-   final record = Record.fromMap(data);
+Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return ListView(
+    padding: const EdgeInsets.only(top: 20.0),
+    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  );
+}
 
-   return Padding(
-     key: ValueKey(record.name),
-     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-     child: Container(
-       decoration: BoxDecoration(
-         border: Border.all(color: Colors.blueGrey),
-         borderRadius: BorderRadius.circular(5.0),
-       ),
-       child: ListTile(
-         title: Text(record.name),
-         trailing: Text(record.votes.toString()),
-         onTap: () => print(record),
-       ),
-     ),
-   );
- }
+Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  final record = Record.fromSnapshot(data);
 
- class Record {
-   final String name;
-   final int votes;
-   final DocumentReference reference;
+  return Padding(
+    key: ValueKey(record.name),
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueGrey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          //  onTap: () => record.reference.updateData({'votes': record.votes + 1}),
+          onTap: () => Firestore.instance.runTransaction((transaction) async {
+                final freshSnapshot = await transaction.get(record.reference);
+                final fresh = Record.fromSnapshot(freshSnapshot);
 
-   Record.fromMap(Map<String, dynamic> map, {this.reference})
-   : assert(map['name'] != null),
-   assert(map['votes'] != null),
-   name = map['name'],
-   votes = map['votes'];
+                await transaction
+                    .update(record.reference, {'votes': fresh.votes + 1});
+              })),
+    ),
+  );
+}
 
-   Record.fromSnapshot(DocumentSnapshot snapshot)
-   :this.fromMap(snapshot.data, reference: snapshot.reference);
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
 
-   @override
-   String toString() => "Record<$name:$votes>";
- }
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
+}
