cc3d0a4ff506e9582f3e3d4326701bc06c311048
phuocding <phuocding@gmail.com>
2018-12-12 16:20:57 +0700

    pushed old app for team free code school app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,182 +6,89 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // build title section row
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Row(
-        children: [
-          // column of text
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    'Gratis Code School',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Hanoi, Vietnam',
-                  style: TextStyle(
-                    color: Colors.orange[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // A star icon, A number
-          FavoriteWidget(),
-        ],
-      ),
-    );
-
-    Column buildButtonColumn(IconData icon, String label) {
-      Color color = Theme.of(context).primaryColor;
-
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: color),
-          Container(
-            margin: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              label,
-              style: TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
-            ),
-          ),
-        ],
-      );
-    }
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          buildButtonColumn(Icons.perm_phone_msg, 'One Call Away'),
-          buildButtonColumn(Icons.directions_run, 'Come with me'),
-          buildButtonColumn(Icons.screen_share, 'Give then Recieve')
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Text(
-        '''One of the best feelings in the world is giving. The positive energy is contagious. With just one act of kindness, you can inspire others to go out and plant seeds of happiness through giving, too. Your greatest gift is the ability to share your blessings; it’s what makes life meaningful. Remember what it means to be a giver with these 10 thoughtful quotes.
-
-        1. “No one has ever become poor by giving.” ―Anne Frank.
-        2. “Remember that the happiest people are not those getting more, but those giving more.” ―H. Jackson Brown Jr.
-        3. “Since you get more joy out of giving joy to others, you should put a good deal of thought into the happiness that you are able to give.” ―Eleanor Roosevelt.
-        4. “We must give more in order to get more. It is the generous giving of ourselves that produces 
-        the generous harvest.” ―Orison Swett Marden.
-        5. “We make a living by what we get. We make a life by what we give.” ―Winston S. Churchill.
-        6. “No one is useless in this world who lightens the burdens of another.” ―Charles Dickens.
-        7. “For it is in giving that we receive.” ―St. Francis of Assisi.
-        8. “Happiness doesn’t result from what we get, but from what we give.” ―Ben Carson.
-        9. “It is every man’s obligation to put back into the world at least the equivalent of what he takes out of it.” ―Albert Einstein.
-        10. “The meaning of life is to find your gift. The purpose of life is to give it away.” ―Pablo Picasso.
-        ''',
-        softWrap: true,
-        style: TextStyle(fontFamily: 'SansForgetica'),
-      ),
-    );
-
     return MaterialApp(
-      title: 'Gratis Code School',
-      theme: ThemeData(
-        // primarySwatch: Colors.amber,
-        primaryColor: Colors.lightBlueAccent[100],
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          title: Text(
-          '{\'GRATIS School\'}',
-           style: TextStyle(color: Colors.red, fontFamily: 'BeynoTtf'),
-           ),
-           actions: <Widget>[
-             IconButton(
-               icon: Icon(Icons.shopping_cart),
-               onPressed: () {},
-             ),
-             IconButton(
-               icon: Icon(Icons.monetization_on),
-               onPressed: () {},
-             ),
-           ],
-        ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              'assets/code.jpg',
-              width: 600.0,
-              height: 240.0,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
-      ),
+      title: 'Free Code School',
+      home: MyHomePage(),
     );
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = false;
-  int _favoriteCount = 149;
-
-  void _toggleFavorite() {
-    setState(() {
-          if (!_isFavorited) {
-            _favoriteCount += 1;
-            _isFavorited = true;
-          } else {
-            _favoriteCount -= 1;
-            _isFavorited = false;
-          }
-        });
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0.0),
-          child: IconButton(
-            icon: (_isFavorited ? Icon(Icons.people) : Icon(Icons.people_outline)),
-            color: Colors.cyan[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          // width: 18.0,
-          child: Container(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: Text('Impressive Person')),
+      body: _buildBody(context),
     );
   }
+}
+
+Widget _buildBody(BuildContext context) {
+  // TODO: get actual snapshot from Cloud Firestore
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('members').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return LinearProgressIndicator();
+
+      return _buildList(context, snapshot.data.documents);
+    },
+  );
+}
+
+Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return ListView(
+    padding: const EdgeInsets.only(top: 20.0),
+    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  );
+}
+
+Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  final record = Record.fromSnapshot(data);
+
+  return Padding(
+    key: ValueKey(record.name),
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueGrey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          //  onTap: () => record.reference.updateData({'votes': record.votes + 1}),
+          onTap: () => Firestore.instance.runTransaction((transaction) async {
+                final freshSnapshot = await transaction.get(record.reference);
+                final fresh = Record.fromSnapshot(freshSnapshot);
+
+                await transaction
+                    .update(record.reference, {'votes': fresh.votes + 1});
+              })),
+    ),
+  );
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
 }
